c216562cef59d8f09c61cb548b992daff0adc23a
Domhouse <sabinashaganova23@gmail.com>
2021-02-09 23:09:39 +0600

    Switch case

---
+++ fourth.php
@@ -0,0 +1,24 @@
+<?php
+$fruits = "apple";
+
+switch ($fruits) {
+  case "apple":
+    echo "Yes , it's an apple!";
+    break;
+  case "strawberry":
+    echo "Yes , it's a strawberry!";
+    break;
+    case "grapes":
+    echo "Yes , it's a grapes!";
+    break;
+    case "cherry":
+    echo "Yes , it's a cherry!";
+    break;
+    case "pear ":
+    echo "Yes , it's a pear !";
+    break;
+  
+  default:
+    echo "You didn't guess ";
+}
+?>
