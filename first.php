056ebc0bfb48ea17b48440eabe2336ee3f46f52e
Domhouse <sabinashaganova23@gmail.com>
2021-02-09 22:27:21 +0600

    Welcome to PHP

---
+++ first.php
@@ -0,0 +1,8 @@
+<?php
+echo "Welcome to PHP!";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "Welcome to PHP!";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+?> 
